4de5fc381a401b324db2c6f2a058d08a52aba4a5
RobStyling <moelrobi@gmail.com>
2016-09-18 00:27:15 +0200

    All Buttons are working!
    Have Fun Playing :dart:

--- Dart/Dart/Form1.Designer.vb
+++ Dart/Dart/Form1.Designer.vb
@@ -825,7 +825,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(652, 741)
+        Me.ClientSize = New System.Drawing.Size(652, 869)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.TextBox4)
